7f969568e9ff8a0014cb2229e4cbf95cdff8bd18
Srivarshan.M <85498112+SriVarshan733@users.noreply.github.com>
2024-03-09 04:31:50 +0000

    cache

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
-import streamlit as st 
+import streamlit as st
+from streamlit_option_menu import option_menu
+from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from pandasai import PandasAI
 
 load_dotenv()
 
+openai_api_key = "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"
+llm = OpenAI(api_token=openai_api_key)
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
-
-def chat_with_csv(df,prompt):
-    llm = OpenAI(api_token=openai_api_key)
+def chat_with_csv(df, prompt):
     pandas_ai = PandasAI(llm)
     result = pandas_ai.run(df, prompt=prompt)
-    print(result)
     return result
 
 st.set_page_config(layout='wide')
 
-st.title("ChatCSV powered by LLM")
+# Create a navigation bar
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Techgpt', 'Help'],
+                           icons=['house', 'terminal', 'question-circle'], menu_icon="cast", default_index=0)
 
-input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+if selected == "Home":
+    st.title("Welcome to TechGPT")
+    st.write("Upload your CSV file and chat with it")
 
-if input_csv is not None:
+elif selected == "Techgpt":
+    st.title("Techgpt")
+    st.write("This is the Techgpt section")
 
-        col1, col2 = st.columns([1,1])
+    input_csv = st.file_uploader("Upload your CSV file", type=['csv'])
+
+    if input_csv is not None:
+
+        col1, col2 = st.columns([1, 1])
 
         with col1:
             st.info("CSV Uploaded Successfully")
@@ -36,12 +45,18 @@ if input_csv is not None:
         with col2:
 
             st.info("Chat Below")
-            
+
             input_text = st.text_area("Enter your query")
 
             if input_text is not None:
                 if st.button("Chat with CSV"):
-                    st.info("Your Query: "+input_text)
+                    st.info("Your Query: " + input_text)
                     result = chat_with_csv(data, input_text)
                     st.success(result)
 
+elif selected == "Help":
+    st.title("Help")
+    st.write("This is the help section")
+    st.write("Upload your CSV file and chat with it")
+    st.write("You can ask questions related to the data in the CSV file")
+    st.write("The TechGPT will answer your questions based on the data in the CSV file")
